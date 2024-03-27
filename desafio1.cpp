5a107585c654b4f68a238331dc5631b11d25de4d
Axelcanizalez <165125501+Axelcanizalez@users.noreply.github.com>
2024-03-27 01:01:45 -0600

    desafio1.cpp

--- desafio1.cpp
+++ desafio1.cpp
@@ -3,10 +3,11 @@
 #include<string>
 using namespace std;
 int main(){
+	//variable a trabajar
 	double hlaborales=0,renta=0.075,isss=0.054,seguro=12,horas,sueldo,total=0;
 	string nombre,apellido,cargo;
 	double total1=0,total2=0;
-	
+	//menu del programa
 	cout<<"\n----------------------------------------------------------------------------";
 	cout<<"\n\t\tCALCULO DE SUELDO FINAL DE EMPLEADO";
 	cout<<"\n---------------------------------------------------------------------------";
@@ -25,9 +26,9 @@ int main(){
 	cin>>hlaborales;
 	cout<<endl;
 	cout<<"\n-----------------------------------------------------------------------------"<<endl;
-	system("pause");
-	system("cls");
-	
+	system("pause");//pausar el programa
+	system("cls");//cambiar de pagina
+	//menu secundario
 	cout<<"\n*****************************************************************************";
 	cout<<"\n\tEmpleado:"<<apellido<<" , "<<nombre;
 	cout<<"\n\tcargo:"<<cargo;
@@ -35,15 +36,15 @@ int main(){
 	
 	cout<<"\nsueldo inicial :$";cin>>sueldo;
 	cout<<"\n(descuento de ley)";
-	total=sueldo*renta,
+	total=sueldo*renta,//haciendo operaciones del sueldo con la resta
 	cout<<"\nRenta    :"<<"$ "<<total;
-	total1=sueldo*isss;
+	total1=sueldo*isss;//haciendo operaciones con isss
 	cout<<"\nISSS    :"<<"$ "<<total1;
-	cout<<"\nSeguro de vida    :"<<"$ "<<seguro;
+	cout<<"\nSeguro de vida    :"<<"$ "<<seguro;//mostrar el valor de seguro
 	cout<<endl;
 	cout<<"\n*******************************************************************************";
-	total2=sueldo-total-total1-seguro;
-	cout<<"\nSueldo a pagar:"<<"$ "<<total2;
+	total2=sueldo-total-total1-seguro;//haciendo calculo para el sueldo a pagar
+	cout<<"\nSueldo a pagar:"<<"$ "<<total2;//mostrando sueldo final
 	
 	
 	
